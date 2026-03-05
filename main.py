f0538224dd32843744914a6bb9f385f9ebed456f
joelhernandez211-id <joelhernandez2115@gmail.com>
2026-03-05 15:57:11 -0500

    commit-1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola Eymi")
+CONFIGURATION ACCORDING TO LEVEL
+
+    if difficulty == "1":
+    oxygen, batteries, spare_parts, health = 100, 100, 100, 100
+
+elif difficulty == "2":
+    oxygen, batteries, spare_parts, health = 95, 90, 90, 90
+
+else:
+    oxygen, batteries, spare_parts, health = 90, 60, 60, 70
