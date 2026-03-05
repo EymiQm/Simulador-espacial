9ab443d53dc51a6d959ed84493fae3b135db967a
MiguelDaza-28 <migueldazah7@gmail.com>
2026-03-05 15:31:21 -0500

    1
    Sistema de consumo de oxigeno y  avance hasta el día 10

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hola Eymi")
+#BUCLE PRINCIPAL
+
+while day <= 10:
+
+    print("\n==============================")
+    print("DAY:", day)
+    print("==============================")
+
+    print("Population:", population)
+    print("Previous Day Status:")
+    print("Health:", health)
+    print("Oxygen:", oxygen)
+    print("Batteries:", batteries)
+    print("Spare Parts:", spare_parts)
+
+    # CONSUMO AUTOMÁTICO
+
+    print("\nCalculating daily consumption...")
+
+    # consumo de oxigeno por persona
+    oxygen_consumption = population * 3
+
+    # aplicar consumo
+    oxygen = oxygen - oxygen_consumption
+
+    # consumo de salud
+    health = health - 5
+
+    # consumo de baterias
+    batteries = batteries - 5
