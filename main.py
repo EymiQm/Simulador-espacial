55970e0353b8adcdae68ca776e1aceeab250cacd
menastiwar689-cmyk <menastiwar689@gmail.com>
2026-03-05 15:56:01 -0500

    Update  1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hola Eymi")
+f oxygen == 0 or health == 0:
+        print("\nGAME OVER ❌")
+        break
+
+    next_day = ""
+
+    while next_day != "1":
+        next_day = input("Press 1 to go to the next day: ")
+        if next_day != "1":
+            print("Invalid option. Press 1 to continue.\n")
+
+    day += 1
