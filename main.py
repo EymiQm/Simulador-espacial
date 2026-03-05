87c0f19845fcf2d04d98ee924e364b76ece276d1
camilomeza14 <cmeza555@hotmail.com>
2026-03-05 18:23:56 -0500

    draft 2 - normal_event and else added

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 # =========================
 # EVENTO NORMAL = Camilo
 # =========================
+else:
 
+        normal_event = random.randint(1, 10)
+
+        if normal_event == 1:
+            print("Normal Event: Beautiful Mars sunrise.")
+
+        elif normal_event == 2:
+            print("Normal Event: Crew plays board games.")
+
+        elif normal_event == 3:
+            print("Normal Event: Communication with Earth successful.")
+
+        elif normal_event == 4:
+            print("Normal Event: Routine system check completed.")
+
+        elif normal_event == 5:
+            print("Normal Event: Crew watches a movie.")
+
+        elif normal_event == 6:
+            print("Normal Event: Minor dust outside the station.")
+
+        elif normal_event == 7:
+            print("Normal Event: Exercise session completed.")
