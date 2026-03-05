a967dca4dca04028618cf31070db13760926675f
andreswhite2026 <andresblancocenteno2@gmail.com>
2026-03-05 15:16:11 -0500

    update 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola Eymi")
+ print(" CURRENT STATUS AFTER EVENT")
+ print("Population:", population)
+ print("Health:", health)
+ print("Oxygen:", oxygen)
+ print("Batteries:", batteries)
+ print("Spare Parts:", spare_parts)
+
+if oxygen < 20 =
+     print(" Oxygen below 20%! Lights and research shut down.")
+     batteries -= 2
