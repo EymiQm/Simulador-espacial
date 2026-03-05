c8c739b1d98a3f2205eddfcdd0461b53e69f6a27
andreswhite2026 <andresblancocenteno2@gmail.com>
2026-03-05 15:21:23 -0500

    update 2

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
- print(" CURRENT STATUS AFTER EVENT")
+MOSTRAR STATUS ACTUAL 
+   
+ print("\n--- CURRENT STATUS AFTER EVENT ---")
  print("Population:", population)
  print("Health:", health)
- print("Oxygen:", oxygen)
- print("Batteries:", batteries)
- print("Spare Parts:", spare_parts)
+    print("Oxygen:", oxygen)
+    print("Batteries:", batteries)
+    print("Spare Parts:", spare_parts)
 
-if oxygen < 20 =
+
+   PRIORIZACIÓN DE SISTEMAS 
+   
+ if oxygen < 20:
      print(" Oxygen below 20%! Lights and research shut down.")
      batteries -= 2
