dab32caf8dca5d315dd1c93018d025a90db9d230
Eymi Quintero <eymi102497@hotmail.com>
2026-03-06 11:33:28 -0500

    Add mission success message for surviving 10 days
    Evitar negativos y final del juego

--- main.py
+++ main.py
@@ -8,3 +8,10 @@
         spare_parts = 0
     if health < 0:
         health = 0
+
+______________________________________
+
+# FINAL DEL JUEGO
+
+if day > 10 and oxygen > 0 and health > 0:
+    print("\nMISSION SUCCESS 🚀 You survived 10 days on Mars!")
