b9c196527ba86a0c78864644dbe79dfe1c2a6ccf
Eymi Quintero <eymi102497@hotmail.com>
2026-03-06 11:30:42 -0500

    Prevent negative resource values in main.py
    Add checks to prevent negative values for resources.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola Eymi")
+# EVITAR NEGATIVOS 
+
+    if oxygen < 0:
+        oxygen = 0
+    if batteries < 0:
+        batteries = 0
+    if spare_parts < 0:
+        spare_parts = 0
+    if health < 0:
+        health = 0
