21d6562ff6df5af4aac41395b196d4a5595ee1e5
andreswhite2026 <andresblancocenteno2@gmail.com>
2026-03-09 11:11:53 -0500

    Update 3
    Updated print statements for better formatting and clarity.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-MOSTRAR STATUS ACTUAL 
+#=======================
+#MOSTRAR STATUS ACTUAL 
+#=======================
    
- print("\n--- CURRENT STATUS AFTER EVENT ---")
- print("Population:", population)
- print("Health:", health)
-    print("Oxygen:", oxygen)
-    print("Batteries:", batteries)
-    print("Spare Parts:", spare_parts)
+ print("\n---- CURRENT STATUS AFTER EVENT ----")
+    print("Population:  ", population)
+    print("Health:      ", health)
+    print("Oxygen:      ", oxygen)
+    print("Batteries:   ", batteries)
+    print("Spare Parts: ", spare_parts)
 
-
-   PRIORIZACIÓN DE SISTEMAS 
+print("----------------------------")
    
  if oxygen < 20:
-     print(" Oxygen below 20%! Lights and research shut down.")
-     batteries -= 2
+     print("⚠️ Oxygen below 20%! Lights and research shut down.")
