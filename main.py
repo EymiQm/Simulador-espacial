2538f3b913d83fc72f91e93afc5f3b9c7c332062
Emanuelx18 <emanuelcueto25@gmail.com>
2026-03-09 11:31:11 -0500

    Commit 3

--- main.py
+++ main.py
@@ -5,3 +5,11 @@
 population = random.randint(4, 6)
 
 print("\nSurvivors assigned to the station:", population)
+
+# =========================
+# DÍA DE LA SEMANA ALEATORIO
+# =========================
+
+days_of_week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+current_day_index = random.randint(0,6)
