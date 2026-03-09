e826cd3c9dea5822bbec20ecb929fd116c3677dc
joyner.r <107435702+Joyner-htm-123@users.noreply.github.com>
2026-03-09 11:14:12 -0500

    Primer commit
    Added difficulty validation for game modes.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hola Eymi")
+import random # ESTE VERSION TIENE EL PORCENTAJE DE EVENTOS CRITICOS DEPENDIENDO DEL MODO DE JUEGO
+
+# =========================
+# VALIDACIÓN DE DIFICULTAD = Joyner
+# =========================
+
+difficulty = ""
+
+while difficulty != "1" and difficulty != "2" and difficulty != "3":
+    print("1. Easy")
+    print("2. Medium")
+    print("3. Hard")
+    
+    difficulty = input("Enter option (1, 2 o 3): ")
+    
+    if difficulty != "1" and difficulty != "2" and difficulty != "3":
+        print("option failed, please enter again.")
